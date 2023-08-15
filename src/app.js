3f4d010f9439924da558fe91869b9fd113c84221
danik3422 <mr.siloacd@gmail.com>
2023-08-15 23:13:51 +0200

    Added gitignore and scrollToTop btn

--- src/app.js
+++ src/app.js
@@ -17,3 +17,19 @@ searchIcon.addEventListener('click', () => {
 	searchInput.classList.toggle('hidden')
 	searchInput.focus()
 })
+
+//Show/Hide Go to Top btn
+const scrollToTopBtn = document.getElementById('scrollToTopBtn')
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 200) {
+		scrollToTopBtn.classList.remove('hidden')
+	} else {
+		scrollToTopBtn.classList.add('hidden')
+	}
+})
+
+//Go to Top
+scrollToTopBtn.addEventListener('click', () => {
+	window.scrollTo({ top: 0, behavior: 'smooth' })
+})
